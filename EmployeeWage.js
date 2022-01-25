c27f85c21e6f2c6cfcf0e29e0a936294e451a1cc
balachandar <govindalapudibalachandar@gmail.com>
2022-01-25 11:55:15 +0530

    Added:Employee_Part_Time file is added

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,22 +1,24 @@
-//uc2 calculate employee parttime  and fulltime
+//uc3 using function
 const Is_Part_Time = 1;
 const Is_Full_time = 2;
 const Part_Time_Hours = 4;
 const Full_Time_Hours = 8;
 const Wage_Per_Hour = 20;
 
-let emphrs=0;
-let empcheck = Math.floor(Math.random () * 10) %3 ;
-switch(empcheck){
+function GetWorkingHours(empcheck){
+switch (empcheck) {
     case Is_Part_Time:
-        emphrs=Part_Time_Hours;
-        break;
+        return Part_Time_Hours;
+      
     case Is_Full_time:
-         emphrs=Full_Time_Hours;
-         break;
-    default :
-         emphrs=0;
-}
+         return Full_Time_Hours;
 
+    default :
+         return 0;
+}
+}
+let emphrs=0;
+let empcheck = Math.floor(Math.random () * 10) % 3;
+emphrs = GetWorkingHours(empcheck);
 let empwage = emphrs * Wage_Per_Hour;
-console.log("UC2-emp wage : "+ empwage);
+console.log("Emp Wage : " +empwage);         
