290e99007f040edf261b8bb6bd659641ad7bf81d
balachandar <govindalapudibalachandar@gmail.com>
2022-01-25 10:19:34 +0530

    Added:Calc_Daily_EmpWage file is added

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,10 +1,22 @@
-//UC1 Checking Employee is Absent Or Present Using Rabdom  Function
-const Is_Absent = 0;
+//uc2 calculate employee parttime  and fulltime
+const Is_Part_Time = 1;
+const Is_Full_time = 2;
+const Part_Time_Hours = 4;
+const Full_Time_Hours = 8;
+const Wage_Per_Hour = 20;
 
-let empcheck = Math.floor(Math.random () * 10)%2;
-if(empcheck == Is_Absent){
-    console.log("Employee Is Absent");
+let emphrs=0;
+let empcheck = Math.floor(Math.random () * 10) %3 ;
+switch(empcheck){
+    case Is_Part_Time:
+        emphrs=Part_Time_Hours;
+        break;
+    case Is_Full_time:
+         emphrs=Full_Time_Hours;
+         break;
+    default :
+         emphrs=0;
 }
-else {
-    console.log("Employee Is Present");
-}
+
+let empwage = emphrs * Wage_Per_Hour;
+console.log("UC2-emp wage : "+ empwage);
