c08682667962a128e0501fea14a56487344724a9
balachandar <govindalapudibalachandar@gmail.com>
2022-01-25 12:35:45 +0530

    Added:EmpWage_For_Month file is added

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,9 +1,10 @@
-//uc3 using function
+//
 const Is_Part_Time = 1;
 const Is_Full_time = 2;
 const Part_Time_Hours = 4;
 const Full_Time_Hours = 8;
 const Wage_Per_Hour = 20;
+const Num_Of_Working_Days = 20;
 
 function GetWorkingHours(empcheck){
 switch (empcheck) {
@@ -17,8 +18,10 @@ switch (empcheck) {
          return 0;
 }
 }
-let emphrs=0;
+let totalemphrs=0;
+for (let day = 0; day < Num_Of_Working_Days; day++){
 let empcheck = Math.floor(Math.random () * 10) % 3;
-emphrs = GetWorkingHours(empcheck);
-let empwage = emphrs * Wage_Per_Hour;
-console.log("Emp Wage : " +empwage);         
+totalemphrs += GetWorkingHours(empcheck);
+}
+let empwage = totalemphrs * Wage_Per_Hour;
+console.log("Total Hrs :"+ totalemphrs + "Emp Wage : " +empwage);          
