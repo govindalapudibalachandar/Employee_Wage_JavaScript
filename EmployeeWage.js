c12406659ee12588a651b1eb5f73efe90ebf4786
balachandar <govindalapudibalachandar@gmail.com>
2022-01-25 09:52:04 +0530

    Added:UC1 EmpAttendce file added

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,3 +1,10 @@
+//UC1 Checking Employee is Absent Or Present Using Rabdom  Function
+const Is_Absent = 0;
 
-
-console.log(" Welcome To Employee Wage Computation Program : ");
+let empcheck = Math.floor(Math.random () * 10)%2;
+if(empcheck == Is_Absent){
+    console.log("Employee Is Absent");
+}
+else {
+    console.log("Employee Is Present");
+}
